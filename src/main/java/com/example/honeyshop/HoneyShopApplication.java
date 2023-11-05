8c2730a9657094c4b2df1ccf906772502ebebc80
hingjae <ljh23270@naver.com>
2023-11-05 19:44:58 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/honeyshop/HoneyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.honeyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/honeyshop/HoneyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honeyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
